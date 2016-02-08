9a2b00afb21c7765c657150acb4f25acb900c632
Thomas Egerer <hakke[underscore]double-oh-seven[at]gmx.de>
2016-02-08 21:36:41 +0100

    ea: Fix script and remove rtmp stuff
    Signed-off-by: Thomas Egerer <hakke[underscore]double-oh-seven[at]gmx.de>

--- ea.pl
+++ ea.pl
@@ -8,20 +8,7 @@ use JSON qw/from_json/;
 use File::Temp qw/mktemp/;
 use Data::Dumper;
 
-use vars qw/@tempfiles $agent $hbbtv $rmp4/;
-
-sub make_rtmp_command(\%) {
-	my $cmd = "";
-
-	foreach my $key (keys %{$_[0]}) {
-		if ('binary' eq $key) {
-			$cmd = "$_[0]->{$key} \\\n" . $cmd;
-		} else {
-			$cmd .= ("" eq $cmd ? "" : " \\\n") . "	--$key $_[0]->{$key}";
-		}
-	}
-	$cmd .= "\n";
-}
+use vars qw/@tempfiles $agent $rmp4/;
 
 # params: 0 url to fetch
 #         1 output file
@@ -44,7 +31,8 @@ sub extract_json_url($) {
 	my ($in, $ret);
 	open $in, "<$_[0]" or die "error opening '$_[0]': $!";
 	while (<$in>) {
-		if ($_ =~ /ALL.json/ and $_ !~ /EXTRAIT/) {
+		#if ($_ =~ /ALL.json/ and $_ !~ /EXTRAIT/) {
+		if ($_ =~ /arte_vp_url_oembed/ and $_ !~ /EXTRAIT/) {
 			if (($ret = $_) =~ s/.*["'](http.*?)["'].*/$1/) {
 				last;
 			}
@@ -52,6 +40,18 @@ sub extract_json_url($) {
 	}
 	close $in;
 	$ret;
+}
+
+# params: 0 tempfile to extract json url from
+# return    json url or undef
+sub extract_real_json_url($) {
+	local $/ = undef;
+	my ($in, $json);
+	open $in, "<$_[0]" or die "error opening '$_[0]': $!";
+	($json = from_json(<$in>)->{'html'}) =~ s/.*?json_url=(.*?)".*/$1/;
+	close $in;
+	while ($json =~ s/\%([a-fA-F0-9]{2})/pack('C', hex($1))/eg) { ; };
+	$json;
 }
 
 sub sanitize_name {
@@ -80,15 +80,6 @@ sub sort_helper($){
 }
 
 sub sort_func($$) {
-	if ($_[0]->{'videoFormat'} =~ /$hbbtv/ and $_[1]->{'videoFormat'} =~ /$hbbtv/ or
-			$_[0]->{'videoFormat'} =~ /$rmp4/ and $_[1]->{'videoFormat'} =~ /$rmp4/) {
-		return $_[0]->{'width'} * $_[0]->{'height'} * $_[0]->{'bitrate'} -
-			$_[1]->{'width'} * $_[1]->{'height'} * $_[1]->{'bitrate'};
-	} elsif ($_[0]->{'videoFormat'} =~ /$hbbtv/ and $_[1]->{'videoFormat'} =~ /$rmp4/) {
-		return -1;
-	} else {
-		return 1;
-	}
 	if ($_[0]->{'versionLibelle'} =~ /UT/ and $_[1]->{'versionLibelle'} =~ /UT/ or
 		$_[0]->{'versionLibelle'} !~ /UT/ and $_[1]->{'versionLibelle'} !~ /UT/) {
 		return sort_helper($_[0]) <=> sort_helper($_[1]);
@@ -97,13 +88,12 @@ sub sort_func($$) {
 }
 
 # params: 0 parse VSR json hash ref (for video stream selection)
-#         1 playpath ref for rtmpdump
-#         2 true for http downloads
-sub choose_playpath($$$) {
-	my ($vsr_hash, $playpath, $http) = @_;
+#         1 http playpath ref for curl/wget
+sub choose_playpath($$) {
+	my ($vsr_hash, $playpath) = @_;
 	my @sorted;
 	foreach (sort keys %{$vsr_hash}) {
-		next if $vsr_hash->{$_}->{'versionLibelle'} =~ /frz/i or $vsr_hash->{$_}->{'mediaType'} =~ /hls/i;
+		next if $vsr_hash->{$_}->{'versionShortLibelle'} =~ /FR/i or $vsr_hash->{$_}->{'mediaType'} !~ /$rmp4/i;
 		push @sorted, $vsr_hash->{$_};
 	}
 	@sorted = sort(sort_func @sorted);
@@ -119,26 +109,19 @@ sub choose_playpath($$$) {
 		$sel = scalar @sorted if ($sel eq "\n");
 	} while ($sel > $i or $sel < 1);
 	--$sel;
-	my $prefix = "";
-	my $vformat = $sorted[$sel]->{'videoFormat'};
-	if ($vformat eq $rmp4) {
-		$prefix = "mp4:"
-	} elsif ($vformat eq $hbbtv) {
-		${$http} = 1;
-	}
-	else {
+	my $vformat = $sorted[$sel]->{'url'};
+	if ($sorted[$sel]->{'mediaType'} ne $rmp4) {
 		die "unknown videoFormat $vformat";
 	}
-	${$playpath} = "\"$prefix$sorted[$sel]->{'url'}\"";
+	${$playpath} = "$sorted[$sel]->{'url'}";
 	$sorted[$sel]->{'quality'}
 }
 
 # params: 0 json file to parse
 #         1 playpath ref for rtmpdump
-#         2 flv (title) ref for video output
-#         3 http true if http download is required
-sub select_video($\$\$\$) {
-	my ($tempfile, $playpath, $rflv, $rhttp) = @_;
+#         2 name (title) ref for video output
+sub select_video($\$\$) {
+	my ($tempfile, $playpath, $rflv) = @_;
 	my $json;
 	{
 		my $in;
@@ -147,12 +130,12 @@ sub select_video($\$\$\$) {
 		$json = from_json(<$in>)->{'videoJsonPlayer'};
 		close $in;
 	}
-	my $quality = choose_playpath($json->{'VSR'}, $playpath, $rhttp);
+	my $quality = choose_playpath($json->{'VSR'}, $playpath);
 	${$rflv} = sanitize_name($json->{'VTI'}, $json->{'VPI'}, $quality);
 }
 
 sub new_tempfile($) {
-	my @steps = ( "main", "json" );
+	my @steps = ( "main", "json", "real_json" );
 	push @tempfiles, mktemp("/tmp/tmp.$_[0].step_$steps[${\(scalar @tempfiles)}].XXXXXXXX");
 	$tempfiles[scalar @tempfiles - 1];
 }
@@ -182,31 +165,22 @@ for my $i (0 .. scalar @ARGV - 1) {
 }
 
 my %config = (
-	'resume'   => '',
-	'binary'   => 'rtmpdump',
-	'app'      => 'a3903/o35/',
-	'swfUrl'   => 'http://www.arte.tv/player/v2//jwplayer6/mediaplayer.6.3.3242.swf',
-	'flashVer' => 'LNX 11,2,202,236',
 );
-$config{'rtmp'} = "rtmp://artestras.fcod.llnwd.net/$config{'app'}";
-$config{'tcUrl'} = "$config{'rtmp'}";
-$config{'swfVfy'} = "$config{'swfUrl'}";
 
 $agent = "User-Agent: Mozilla/5.0 (X11; Linux i686; rv:32.0) Gecko/20100101 Firefox/32.0";
-$hbbtv = "HBBTV";
-$rmp4 = "RMP4";
+$rmp4 = "mp4";
 my $tempfile = new_tempfile($basename);
 curl($ARGV[0], $tempfile);
 my $json_url = extract_json_url($tempfile);
 $tempfile = new_tempfile($basename);
 curl($json_url, $tempfile);
+my $real_json_url = extract_real_json_url($tempfile);
+$tempfile = new_tempfile($basename);
+curl($real_json_url, $tempfile);
 
-select_video($tempfile, $config{'playpath'}, $config{'flv'}, $config{'http'});
+select_video($tempfile, $config{'http'}, $config{'name'});
 if (defined $config{'http'}) {
-	print `curl -C - -L -A '$agent' $config{'playpath'} -o $config{'flv'}`;
-} else {
-	delete $config{'http'};
-	my $cmd = make_rtmp_command(%config);
+	my $cmd = "curl -C - -L -A '$agent' $config{'http'} -o $config{'name'}";
 	if ($dry) {
 		print $cmd
 	} else {
